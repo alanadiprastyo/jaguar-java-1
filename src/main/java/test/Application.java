86a486ff04d8cc761d7696b01776bdfd4831e692
Alan Adi Prastyo <alanprastyo95@gmail.com>
2021-02-05 09:18:41 +0700

    update code version 3.4

--- src/main/java/test/Application.java
+++ src/main/java/test/Application.java
@@ -15,7 +15,7 @@ public class Application {
 	
 	@RequestMapping(value="/")
 	public String Demo(){
-		return "Programming Hello World - version 3.3";
+		return "Programming Hello World - version 3.4";
 	}
 
         @RequestMapping(value="/about")
